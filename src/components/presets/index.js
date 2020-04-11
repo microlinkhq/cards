a47fc151de5093b829d119ca337041a1bd7a8df1
GermanRodrickson <germanrodrickson@gmail.com>
2020-04-11 17:42:16 +0200

    feat: add fauna template

--- src/components/presets/index.js
+++ src/components/presets/index.js
@@ -2,10 +2,12 @@ import zeit from './zeit'
 import simple from './simple'
 import article from './article'
 import thePracticalDev from './thePracticalDev'
+import fauna from './fauna'
 
 export default {
   simple,
   zeit,
   article,
-  thePracticalDev
+  thePracticalDev,
+  fauna
 }
