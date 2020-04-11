933857ff3844312ad143c1e0b43c757c527e98fc
GermanRodrickson <germanrodrickson@gmail.com>
2020-04-11 22:01:06 +0200

    feat: add news template

--- src/components/presets/index.js
+++ src/components/presets/index.js
@@ -3,11 +3,13 @@ import simple from './simple'
 import article from './article'
 import thePracticalDev from './thePracticalDev'
 import fauna from './fauna'
+import news from './news'
 
 export default {
   simple,
   zeit,
   article,
   thePracticalDev,
-  fauna
+  fauna,
+  news
 }
