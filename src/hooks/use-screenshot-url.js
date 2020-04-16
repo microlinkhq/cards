3c2785d6361a692b7062fe96c0b0c4c2bf6eb9e4
Kiko Beats <josefrancisco.verdu@gmail.com>
2020-04-16 10:48:38 +0200

    refactor: use URL object

--- src/hooks/use-screenshot-url.js
+++ src/hooks/use-screenshot-url.js
@@ -6,13 +6,14 @@ export default queryVariables => {
   const [screenshotUrl, setScreenshotUrl] = useState('')
 
   const sync = queryVariables => {
+    const urlObj = new URL(window.location)
+    urlObj.pathname = ''
+    const url = decodeURI(urlObj.toString())
+
     setScreenshotUrl(
-      getScreenshotUrl(
-        decodeURI(window.location.href.replace('/editor/', '')),
-        {
-          endpoint: isDev ? 'http://localhost:3000' : queryVariables.endpoint
-        }
-      )
+      getScreenshotUrl(url, {
+        endpoint: isDev ? 'http://localhost:3000' : queryVariables.endpoint
+      })
     )
   }
 
