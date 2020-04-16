bbd80c50ea6c9d9aca68a98f8f37db77b20f8094
Kiko Beats <josefrancisco.verdu@gmail.com>
2020-04-16 12:39:51 +0200

    build: use shallow router

--- src/hooks/use-loading.js
+++ src/hooks/use-loading.js
@@ -1,17 +1,15 @@
 import { useState, useEffect } from 'react'
 import isEmpty from '@/lib/is-empty'
 import Router from 'next/router'
-import defer from 'tickedoff'
 
 export default () => {
   const [isLoading, setIsLoading] = useState(true)
 
   useEffect(() => {
     if (Router.asPath === '/' && isEmpty(Router.query)) {
-      return Router.push({ pathname: '/editor' })
+      return Router.push('/editor', '/editor', { shallow: true })
     }
     setIsLoading(false)
-    // defer(() => setIsLoading(false))
   }, [])
 
   return isLoading
