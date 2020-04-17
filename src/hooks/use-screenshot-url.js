b8419af5da4fc8ea5e2d27f45947e06cd7fd148c
Kiko Beats <josefrancisco.verdu@gmail.com>
2020-04-17 18:18:24 +0200

    build(debug): restore logic

--- src/hooks/use-screenshot-url.js
+++ src/hooks/use-screenshot-url.js
@@ -1,10 +1,10 @@
-// import getScreenshotUrl from '@/lib/screenshot-url'
+import getScreenshotUrl from '@/lib/screenshot-url'
 import { useState, useEffect } from 'react'
-// import isDev from '@/lib/is-dev'
+import isDev from '@/lib/is-dev'
 
-// const shortenUrl = isDev
-//   ? 'http://localhost:3000/?adblock=false&element=%23screenshot&embed=screenshot.url&meta=false&screenshot&waitUntil.0=load&waitUntil.1=networkidle0&url='
-//   : 'https://i.microlink.io/'
+const shortenUrl = isDev
+  ? 'http://localhost:3000/?adblock=false&element=%23screenshot&embed=screenshot.url&meta=false&screenshot&waitUntil.0=load&waitUntil.1=networkidle0&url='
+  : 'https://i.microlink.io/'
 
 const getUrl = () => {
   const urlObj = new URL(window.location)
@@ -13,20 +13,20 @@ const getUrl = () => {
 }
 
 const getCardUrl = ({ queryVariables }) => {
-  return `https://i.microlink.io/${encodeURIComponent(getUrl())}`
-
-  // const { endpoint } = queryVariables
-  // if (!endpoint) return shortenUrl + encodeURIComponent(getUrl())
-  // return getScreenshotUrl(getUrl(), {
-  //   force: !!isDev,
-  //   endpoint: isDev ? 'http://localhost:3000' : endpoint,
-  //   adblock: false,
-  //   element: '#screenshot',
-  //   embed: 'screenshot.url',
-  //   meta: false,
-  //   screenshot: true,
-  //   waitUntil: ['load', 'networkidle0']
-  // })
+  const { endpoint } = queryVariables
+  if (!isDev && !endpoint) {
+    return `${shortenUrl}/${encodeURIComponent(getUrl())}`
+  }
+  return getScreenshotUrl(getUrl(), {
+    force: !!isDev,
+    endpoint: isDev ? 'http://localhost:3000' : endpoint,
+    adblock: false,
+    element: '#screenshot',
+    embed: 'screenshot.url',
+    meta: false,
+    screenshot: true,
+    waitUntil: ['load', 'networkidle0']
+  })
 }
 
 export default opts => {
