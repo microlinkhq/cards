4f618582c1c15951faadf5c3f12d4ca03512b6e9
Kiko Beats <josefrancisco.verdu@gmail.com>
2020-04-17 18:00:03 +0200

    build: avoid redirect

--- src/hooks/use-screenshot-url.js
+++ src/hooks/use-screenshot-url.js
@@ -2,9 +2,9 @@ import getScreenshotUrl from '@/lib/screenshot-url'
 import { useState, useEffect } from 'react'
 import isDev from '@/lib/is-dev'
 
-const shortenUrl = isDev
-  ? 'http://localhost:3000/?adblock=false&element=%23screenshot&embed=screenshot.url&meta=false&screenshot&waitUntil.0=load&waitUntil.1=networkidle0&url='
-  : 'https://i.microlink.io/'
+// const shortenUrl = isDev
+//   ? 'http://localhost:3000/?adblock=false&element=%23screenshot&embed=screenshot.url&meta=false&screenshot&waitUntil.0=load&waitUntil.1=networkidle0&url='
+//   : 'https://i.microlink.io/'
 
 const getUrl = () => {
   const urlObj = new URL(window.location)
@@ -14,9 +14,10 @@ const getUrl = () => {
 
 const getCardUrl = ({ queryVariables }) => {
   const { endpoint } = queryVariables
-  if (!endpoint) return shortenUrl + encodeURIComponent(getUrl())
+  // if (!endpoint) return shortenUrl + encodeURIComponent(getUrl())
   return getScreenshotUrl(getUrl(), {
-    endpoint: endpoint,
+    force: !!isDev,
+    endpoint: endpoint || 'http://localhost:3000',
     adblock: false,
     element: '#screenshot',
     embed: 'screenshot.url',
