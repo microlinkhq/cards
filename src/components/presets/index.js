dcbcb502f18d740b38940337c848e12eb77a4275
Kiko Beats <josefrancisco.verdu@gmail.com>
2020-04-18 11:21:16 +0200

    build: add bytesandhumans template

--- src/components/presets/index.js
+++ src/components/presets/index.js
@@ -2,12 +2,14 @@ import zeit from './zeit'
 import simple from './simple'
 import article from './article'
 import rauchg from './rauchg'
+import bytesandhumans from './bytesandhumans'
 import thePracticalDev from './thePracticalDev'
 import fauna from './fauna'
 import news from './news'
 
 export default {
   article,
+  bytesandhumans,
   fauna,
   news,
   rauchg,
