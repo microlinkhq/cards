1f6dd2d1ce5176a0fb895c506d07d943fa1ef6e7
Kiko Beats <josefrancisco.verdu@gmail.com>
2020-04-23 01:18:31 +0200

    build: adjust border

--- src/components/button.js
+++ src/components/button.js
@@ -4,7 +4,7 @@ export default ({ sx, ...props }) => (
   <Button
     sx={{
       cursor: 'pointer',
-      borderRadius: 2,
+      borderRadius: 4,
       py: ['4px', '4px', '8px', '8px'],
       px: ['6px', '6px', '12px', '12px'],
       outline: 0,
