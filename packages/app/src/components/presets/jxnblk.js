8c5e8ebab8cfd2cf306a6840a08cb524a58ee37d
Kiko Beats <josefrancisco.verdu@gmail.com>
2020-05-01 09:34:54 +0000

    build: add jxnblk preset
    closes #39

---
+++ packages/app/src/components/presets/jxnblk.js
@@ -0,0 +1,53 @@
+/* eslint-disable no-use-before-define */
+
+import Inline from '../inline.macro'
+import { Image, Flex, Text } from './scope'
+
+const code = (
+  <Inline>
+    <>
+      <Flex
+        sx={{
+          alignItems: 'center',
+          justifyContent: 'center',
+          flexDirection: 'column',
+          bg: query.bg,
+          px: 5
+        }}
+      >
+        <Image sx={{ width: '128px' }} src={query.logo} />
+        <Text
+          sx={{
+            py: 4,
+            textTransform: 'uppercase',
+            letterSpacing: '0.2em',
+            lineHeight: 1.25,
+            fontFamily: 'sans-serif',
+            fontWeight: 800,
+            fontSize: 4,
+            color: query.color
+          }}
+          children={query.title}
+        />
+        <Text
+          sx={{
+            fontWeight: 500,
+            fontSize: 3,
+            color: query.color
+          }}
+          children={query.subtitle}
+        />
+      </Flex>
+    </>
+  </Inline>
+)
+
+const query = {
+  bg: 'white',
+  color: 'black',
+  logo: 'https://raw.githubusercontent.com/jxnblk/avatar/master/avatar.svg',
+  title: 'TWO STEPS FORWARD, ONE STEP BACK',
+  subtitle: 'jxnblk.com'
+}
+
+export const jxnblk = { name: 'jxnblk', code, query }
