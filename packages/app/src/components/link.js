3c0b846dc1eccdaa7193fb1352591a25449b0ad6
Kiko Beats <josefrancisco.verdu@gmail.com>
2020-05-11 20:08:50 +0200

    build: avoid unmount injected links

--- packages/app/src/components/link.js
+++ packages/app/src/components/link.js
@@ -5,7 +5,6 @@ export const Link = props => {
     const link = document.createElement('link')
     Object.keys(props).forEach(key => (link[key] = props[key]))
     document.head.appendChild(link)
-    return () => document.head.removeChild(link)
   }, [])
   return false
 }
