12bccc6200094ba671d3de3e8e3a98d247b4fd7f
Javier Álvarez Medina <jmedina@tinybird.co>
2020-11-26 16:06:03 +0100

    build(preset): tinybird preset

---
+++ packages/app/src/components/presets/tinybird.js
@@ -0,0 +1,95 @@
+/* eslint-disable */
+
+import Inline from '../inline.macro'
+import { Image, Flex, Text } from './scope'
+
+const code = (
+  <Inline>
+    <>
+      <Flex
+        sx={{
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+          bg: '#25283D',
+          pl: 5,
+          pr: 5
+        }}
+      >
+        <Link
+          href='https://fonts.googleapis.com/css2?family=Inter:wght@400&display=swap'
+          rel='stylesheet'
+        />
+
+        <Flex
+          as='header'
+          sx={{
+            position: 'absolute',
+            top: 0,
+            left: 5,
+            display: 'flex',
+            alignItems: 'center',
+            width: '100%',
+            mt: '3em'
+          }}
+        >
+          <Image
+            sx={{
+              width: '26px'
+            }}
+            src='https://storage.googleapis.com/tinybird-assets/img/logo-inverted.svg'
+          />
+          <Text
+            sx={{
+              pl: 3,
+              display: 'inline',
+              fontWeight: 400,
+              fontSize: '22px',
+              fontFamily: 'Inter',
+              color: '#FFF',
+              lineHeight: '140%'
+            }}
+          >
+            Tinybird
+            {query.subtitle && (
+              <span
+                style={{
+                  display: 'inline',
+                  fontWeight: 400,
+                  fontSize: '22px',
+                  fontFamily: 'Inter',
+                  color: '#1FCC83',
+                  lineHeight: '140%'
+                }}
+              >
+                /{query.subtitle}
+              </span>
+            )}
+          </Text>
+        </Flex>
+
+        <Flex as='content'>
+          <Text
+            sx={{
+              display: 'inline',
+              fontWeight: 400,
+              fontSize: '42px',
+              fontFamily: 'Inter',
+              color: '#FFF',
+              lineHeight: '140%'
+            }}
+          >
+            {query.text}
+          </Text>
+        </Flex>
+      </Flex>
+    </>
+  </Inline>
+)
+
+const query = {
+  subtitle: 'guides',
+  text: 'Learn how to accelerate real-time analytics'
+}
+
+export const tinybird = { name: 'tinybird', code, query }
