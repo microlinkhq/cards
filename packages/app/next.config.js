4c9202dc61e063e5a4b2100012328960ee41f3ce
Kiko Beats <josefrancisco.verdu@gmail.com>
2021-01-29 22:35:49 +0100

    build: remove next-offline

--- packages/app/next.config.js
+++ packages/app/next.config.js
@@ -1,5 +1,4 @@
 const withTM = require('next-transpile-modules')(['@kikobeats/use-query-state'])
-const withOffline = require('next-offline')
 const nextConfig = {}
 
-module.exports = withTM(withOffline(nextConfig))
+module.exports = withTM(nextConfig)
