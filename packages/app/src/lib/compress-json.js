e04c8d6698e8cf1c96317bcd2d500d0299b84a91
Kiko Beats <josefrancisco.verdu@gmail.com>
2021-03-26 12:51:04 +0100

    build: update dependencies

--- packages/app/src/lib/compress-json.js
+++ packages/app/src/lib/compress-json.js
@@ -1,7 +1,7 @@
-import Msgpack from 'msgpack5'
+import createMsgpack from 'msgpack5'
 import URLSafeBase64 from 'urlsafe-base64'
 
-const msgpack = new Msgpack()
+const msgpack = createMsgpack()
 
 export const marshall = value => URLSafeBase64.encode(msgpack.encode(value))
 
