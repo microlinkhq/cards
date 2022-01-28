6f434f562d69671c86af4c13d21d819e02f066b9
Brad Adams <hi@breadadams.com>
2022-01-28 22:48:35 +0100

    fix: remove `FALLBACK_SLUGS` logic

--- packages/app/src/constants.js
+++ packages/app/src/constants.js
@@ -29,16 +29,3 @@ export const OVERLAY_STATE = {
   ABOUT: 'about',
   KEYBINDINGS: 'keybindings'
 }
-
-export const FALLBACK_SLUGS = {
-  bytesandhumans: 'bytes-and-humans',
-  chrisbiscardi: 'chris-biscardi',
-  draculashop: 'dracula-shop',
-  kfund: 'k-fund',
-  meta: 'microlink-sdk',
-  nextjsconf: 'nextjs-conf',
-  richhaines: 'rich-haines',
-  thebootstrappedfounder: 'the-bootstrapped-founder',
-  thepracticaldev: 'the-practical-dev',
-  wesbos: 'wes-bos'
-}
