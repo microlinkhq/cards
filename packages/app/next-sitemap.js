033647748e9a4eeea227592819348f699a73199e
Kiko Beats <josefrancisco.verdu@gmail.com>
2022-01-29 14:06:45 +0100

    ci: adjust scripts

--- packages/app/next-sitemap.js
+++ packages/app/next-sitemap.js
@@ -3,8 +3,7 @@ const siteUrl = process.env.NEXT_PUBLIC_VERCEL_URL
   ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
   : require('./package.json').homepage
 
-console.log('Generating sitemap for', siteUrl)
-console.log('debug', process.env)
+console.log('Sitemap will generated for', siteUrl)
 
 module.exports = {
   siteUrl: process.env.SITE_URL || siteUrl,
