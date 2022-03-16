bfa11f96779cc4d9e29e81cf47c91f393727e9f2
Kiko Beats <josefrancisco.verdu@gmail.com>
2022-03-16 22:52:18 +0100

    refactor: tweaks over sitemap generation

--- packages/app/next-sitemap.js
+++ packages/app/next-sitemap.js
@@ -2,9 +2,15 @@ const siteUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
   : require('./package.json').homepage
 
-console.log('Sitemap will generated for', siteUrl)
-
 module.exports = {
   siteUrl: process.env.SITE_URL || siteUrl,
-  generateRobotsTxt: true
+  generateRobotsTxt: true,
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: '*',
+        allow: '/'
+      }
+    ]
+  }
 }
