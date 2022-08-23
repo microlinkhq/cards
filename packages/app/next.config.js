bf3b339ddda9971be33aeb3a4d1b614c81342c21
Kiko Beats <josefrancisco.verdu@gmail.com>
2022-08-23 15:15:13 +0200

    build: update dependencies

--- packages/app/next.config.js
+++
@@ -1,9 +0,0 @@
-const withTM = require('next-transpile-modules')([
-  '@kikobeats/use-query-state',
-  'cycled',
-  'react-aspect-ratio'
-])
-
-const nextConfig = {}
-
-module.exports = withTM(nextConfig)
