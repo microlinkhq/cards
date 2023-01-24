84ba37ec4a355ea3990436365eb1fcd052e5a81d
Kiko Beats <josefrancisco.verdu@gmail.com>
2023-01-24 17:06:53 +0100

    refactor: font swap

--- packages/app/src/pages/_document.js
+++ packages/app/src/pages/_document.js
@@ -38,7 +38,7 @@ export default class MyDocument extends Document {
           <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin />
           <link rel='preconnect' href='https://cdn.microlink.io' />
           <link
-            href='https://fonts.googleapis.com/css2?family=Inter:wght@100;200;400;500;600&display=optional'
+            href='https://fonts.googleapis.com/css2?family=Inter:wght@100;200;400;500;600&display=swap'
             rel='stylesheet'
           />
           {/* <!-- Basic --> */}
