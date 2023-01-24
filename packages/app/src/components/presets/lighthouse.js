e2a88aa1337a7711bb8847b1989e4871ff85ae2f
Kiko Beats <josefrancisco.verdu@gmail.com>
2023-01-24 09:56:14 +0100

    refactor: remove duplicate code

--- packages/app/src/components/presets/lighthouse.js
+++ packages/app/src/components/presets/lighthouse.js
@@ -1,50 +1,5 @@
 import Inline from '../inline.macro'
-import { Progress, Text, Link, Flex } from './scope'
-
-const getColors = (value, theme) => {
-  if (value >= 90) {
-    return {
-      primary: '#0CCE6B',
-      secondary: theme === 'dark' ? '#1F3228' : '#E7FAF0'
-    }
-  }
-
-  if (value >= 50) {
-    return {
-      primary: '#FFA400',
-      secondary: theme === 'dark' ? '#372E1E' : '#FFF6E6'
-    }
-  }
-  return {
-    primary: '#FF4E42',
-    secondary: theme === 'dark' ? '#372624' : '#FFEEEC'
-  }
-}
-
-const Lighthouse = ({ value, label, theme }) => {
-  return (
-    <Flex
-      sx={{
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center'
-      }}
-    >
-      <Progress value={value} {...getColors(value, theme)} />
-      <Text
-        sx={{
-          mt: '10px',
-          fontWeight: 300,
-          fontFamily: 'Roboto, Helvetica, Arial, sans-serif',
-          color: theme === 'dark' ? '#F5F5F5' : '#212121',
-          fontSize: '24px'
-        }}
-      >
-        {label}
-      </Text>
-    </Flex>
-  )
-}
+import { Lighthouse, Text, Link, Flex } from './scope'
 
 const query = {
   bg: {
