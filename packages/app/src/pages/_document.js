105554b6f1ece09b6d66aa3ca79008a0e5c1ab24
Kiko Beats <josefrancisco.verdu@gmail.com>
2023-06-08 11:30:47 +0000

    fix: react warning

--- packages/app/src/pages/_document.js
+++ packages/app/src/pages/_document.js
@@ -35,7 +35,11 @@ export default class MyDocument extends Document {
         <Head>
           <meta charSet='utf-8' />
           <link rel='preconnect' href='https://fonts.googleapis.com' />
-          <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin />
+          <link
+            rel='preconnect'
+            href='https://fonts.gstatic.com'
+            crossOrigin='true'
+          />
           <link rel='preconnect' href='https://cdn.microlink.io' />
           <link
             href='https://fonts.googleapis.com/css2?family=Inter:wght@100;200;400;500;600&display=swap'
